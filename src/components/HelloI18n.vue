138f8a75ca6c7078352393ef01a14b92f4e68587
Lucian Daniliuc <dlucian@gmail.com>
2019-06-06 18:36:33 +0300

    Revert "update i18n configure"

--- src/components/HelloI18n.vue
+++ src/components/HelloI18n.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h3>Hello I18n</h3>
-    <p>SFC messsage: {{ $t('message.hello') }}</p>
-    <p>locales message: {{ $t('message.message') }}</p>
+    <p>SFC messsage: {{ $t('hello') }}</p>
+    <p>locales message: {{ $t('message') }}</p>
   </div>
 </template>
 
@@ -11,3 +11,11 @@ export default {
   name: 'HelloI18n',
 };
 </script>
+
+<i18n>
+{
+  "en": {
+    "hello": "Hello i18n in SFC!"
+  }
+}
+</i18n>
